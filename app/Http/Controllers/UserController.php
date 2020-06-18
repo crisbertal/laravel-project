71912ea05b22f72791e67e93d8e6d3cc055580ab
Cristobal Bergillos <cristobalbergillostal@gmail.com>
2020-06-18 15:44:45 +0200

    All raw sql queries

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,13 +8,25 @@ use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
     public function index() {
+        /**
+         * RAW query
+         */
+
         //DB::insert('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [
             //'Cristo',
             //'cristo@gmail.com',
             //'pass',
         //]);
 
-        $users = DB::select('SELECT * FROM users');
-        return $users;
+        //$users = DB::select('SELECT * FROM users');
+        //return $users;
+
+        //DB::update('UPDATE users SET name = ? WHERE id = 1', [
+            //'Edu',
+        //]);
+
+        //DB::delete('DELETE FROM users');
+
+        return view('home');
     }
 }
