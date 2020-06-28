0a46986de6815f8f946f13e011d452439768f4cc
Cristobal Bergillos <cristobalbergillostal@gmail.com>
2020-06-28 12:55:46 +0200

    Field validation

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
 {
@@ -18,6 +19,31 @@ class TodoController extends Controller
     }
 
     public function store(Request $request) {
+        /*
+            validacion de campos
+        */
+        //$request->validate([
+            // obligatorio y con longitud inferior a 255 char
+            //'title' => 'required|max:255',
+        //]);
+
+        /*
+            otra forma de validar
+        */
+        $rules = [
+            'title' => 'required|max:255',
+        ];
+
+        $messages = [
+            'title.max' => 'Todo title should not be greater than 255 chars',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
         Todo::create($request->all());
         // deuelve hacia atras para que no se quede la pagina en blanco
         return redirect()->back()->with('message', 'Todo Created Successfully');
