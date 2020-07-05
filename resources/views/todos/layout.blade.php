2c440e651af1a84a7434e401647b3b677ce77cce
Cristobal Bergillos <cristobalbergillostal@gmail.com>
2020-07-05 16:30:48 +0200

    Changes in UI if task is completed

--- resources/views/todos/layout.blade.php
+++ resources/views/todos/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/js/all.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <title>Todos</title>
 </head>
 <body>
