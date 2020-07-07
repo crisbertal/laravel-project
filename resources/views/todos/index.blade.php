e2243476e02a79ceb16544f2e086faf955eb8ba7
Cristobal Bergillos <cristobalbergillostal@gmail.com>
2020-07-07 16:26:18 +0200

    Changed place of complete button

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -11,6 +11,9 @@
     <x-alert />
     @foreach ($todos as $todo)
     <li class="flex justify-between py-2">
+        <div>
+            @include('todos.complete-button')
+        </div>
         @if ($todo->completed)
             <p class="line-through"> {{ $todo->title }} </p>
         @else
@@ -18,29 +21,6 @@
         @endif
        <div>
             <a href="{{'/todos/' . $todo->id . '/edit'}}" class="py-1 cursor-pointer rounded" > <span class="fas fa-edit text-orange-400 px-2"/></a>
-            @if ($todo->completed)
-                <span class="fas fa-check text-green-400 cursor-pointer px-2" onclick="event.preventDefault();
-                                document.getElementById('form-incomplete-{{$todo->id}}')
-                                .submit()"/>
-
-                <!-- Este formulario esta oculto -->
-                <form style="display: none" id="{{'form-incomplete-'.$todo->id}}" method="post" action="{{ route('todo.incomplete', $todo->id) }}">
-                    <!-- Yo usaria patch porque solo se quiere cambiar el atributo complete del modelo Todo -->
-                    @csrf
-                    @method('put')
-                </form>
-            @else
-                <span class="fas fa-check text-gray-300 cursor-pointer px-2" onclick="event.preventDefault();
-                                document.getElementById('form-complete-{{$todo->id}}')
-                                .submit()"/>
-
-                <!-- Este formulario esta oculto -->
-                <form style="display: none" id="{{'form-complete-'.$todo->id}}" method="post" action="{{ route('todo.complete', $todo->id) }}">
-                    <!-- Yo usaria patch porque solo se quiere cambiar el atributo complete del modelo Todo -->
-                    @csrf
-                    @method('put')
-                </form>
-            @endif
        </div>
     </li>
     @endforeach
