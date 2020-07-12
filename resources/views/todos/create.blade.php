8dfd66a64c7f529cb14bf416df1718f8d87e8174
Cristobal Bergillos <cristobalbergillostal@gmail.com>
2020-07-12 17:04:42 +0200

    Intro to Laravel Livewire

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -5,13 +5,21 @@
     <x-alert />
     <form method="post" action="{{ route('todos.store') }}" class="py-5">
         @csrf
-        <div class="p-1">
+        <div class="py-1">
             <input type="text" name="title" class="p-2 border rounded" placeholder="Title"/>
         </div>
-        <div class="p-1">
+        <div class="py-1">
             <textarea name="description" class="p-2 border rounded" placeholder="Description"></textarea>
         </div>
-        <div class="p-1">
+
+        <div class="py-2 flex justify-center">
+            <h2>Add steps</h2>
+            <span class="px-3 py-1 fas fa-plus"/>
+        </div>
+
+        <input type="text" name="step" class="p-2 border rounded" placeholder="Steps"/>
+
+        <div class="py-1">
             <input type="submit" value="Create" class="p-2 border rounded"/>
         </div>
     </form>
